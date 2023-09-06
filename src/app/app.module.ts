70d64b4d64346a3b4c04b627856a99975bd1bc30
rajshri-kolhe <kolherajshri2@gmail.com>
2023-09-06 17:51:11 +0530

    template-isin-wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { CoreModuleModule } from './core-module/core-module.module';
+import { HomeComponent } from './component/home/home.component';
+import { NavbarComponent } from './component/home/navbar/navbar.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    CoreModuleModule,
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
